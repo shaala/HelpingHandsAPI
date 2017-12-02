e2ffa3c3715febf8b0520a36dfe88ad785600ba8
fox65 <fox65@email.franklin.edu>
2017-12-01 21:02:19 -0800

    user account

---
+++ RSRCapi/api/models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    email: String,
+    firstName: String,
+    lastName: String,
+    passwordHash: String,
+    passwordSalt: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
