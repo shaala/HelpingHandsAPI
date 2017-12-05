659c5093c42f95573c217ca2194b9719c98574d4
fox65 <fox65@email.franklin.edu>
2017-12-04 21:04:32 -0800

    category and route changes

--- RSRCapi/server.js
+++ RSRCapi/server.js
@@ -1,23 +1,25 @@
 var express = require('express'),
   app = express(),
-  var sql = require('mysql');
+
   port = process.env.PORT || 8100,
-   Task = require('./api/models/doneModel'), //created model loading here
-   bodyParser = require('body-parser');
 
- // mongoose instance connection url connection
- mysql.Promise = global.Promise;
- mysql.connect('104.198.103.85');
-
-
- app.use(bodyParser.urlencoded({ extended: true }));
- app.use(bodyParser.json());
-
-
- var routes = require('./api/routes/doneRoutes'); //importing route
- routes(app); //register the route
+ //  app.listen(port);
+ //   Task = require('./api/models/rsrcModel'), //created model loading here
+ //   bodyParser = require('body-parser');
+ //
+ // // mongoose instance connection url connection
+ // mysql.Promise = global.Promise;
+ // mysql.connect('104.198.103.85');
+ //
+ //
+ // app.use(bodyParser.urlencoded({ extended: true }));
+ // app.use(bodyParser.json());
+ //
+ //
+ // var routes = require('./api/routes/doneRoutes'); //importing route
+ // routes(app); //register the route
 
 
  app.listen(port);
 
-console.log('Done RESTful API server started on: ' + 8100);
+console.log('Done RESTful API server started on: ' + port);
