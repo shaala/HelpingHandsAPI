0e435677895dc2b9a1928d1eb3fd654e460eacb9
sfitzgerald16 <misfitz616@gmail.com>
2017-12-12 11:00:09 -0800

    added signup route

--- RSRCapi/api/routes/users.js
+++ RSRCapi/api/routes/users.js
@@ -35,6 +35,49 @@ module.exports = (router) => {
             }
         });
     });
+// REGISTER NEW USER
+    router.post('/users/signup', (req, res) => {
+        User.findOne({ email: req.body.email }, (err, user) => {
+            if (user) {
+                res.json({ success: false, message: 'User already exists. '});
+            } else {
+                if (!req.body.email) {
+                    res.json({ success: false, message: 'Email is required.'});
+                } else {
+                    if (!req.body.password) {
+                        res.json({ success: false, message: 'Password is required.' });
+                    } else {
+                        if (!req.body.firstName) {
+                            res.json({ success: false, message: 'First name is required.' });
+                        } else {
+                            if (!req.body.lastName) {
+                                res.json({ success: false, message: 'Last name is required.' });
+                            } else {
+                                if (!req.body.zipCode) {
+                                    res.json({ success: false, message: 'Zip code is required.' });
+                                } else {
+                                    const user = new User({
+                                        email: req.body.email,
+                                        firstName: req.body.firstName,
+                                        lastName: req.body.lastName,
+                                        password: req.body.password,
+                                        zipCode: req.body.zipCode
+                                    });
+                                    user.save((err) => {
+                                        if (err) {
+                                            res.json({ success: false, message: err });
+                                        } else {
+                                            res.json({ success: true, message: 'User saved!' });
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    });
     return router;
 }
 
