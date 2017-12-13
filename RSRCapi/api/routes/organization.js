f3c3a6a5cf480b43fd05fb392dd34b177baf51b0
Mia Boyles <hdandmb@gmail.com>
2017-12-12 16:59:04 -0800

    working create organization

--- RSRCapi/api/routes/organization.js
+++ RSRCapi/api/routes/organization.js
@@ -8,6 +8,13 @@ module.exports = (router) => {
         var org = new Org();
         org.name = req.body.name;
         org.password = req.body.password;
+        org.email = req.body.email;
+        org.address = req.body.address;
+        org.city = req.body.city;
+        org.state = req.body.state;
+        org.zipCode = req.body.zipCode;
+        org.phone = req.body.phone;
+        org.taxIdNumber = req.body.taxIdNumber;
     
         //save org and err check
         org.save(function(err, org) {
@@ -18,9 +25,11 @@ module.exports = (router) => {
         });
         
     });
+
+    
 return router;
 
-};
+}
 
 
  
